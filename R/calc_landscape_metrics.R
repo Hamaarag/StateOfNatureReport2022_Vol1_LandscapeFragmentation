d0e372269867b8e8f2ea654f56c7664fd7b9da24
Ron Chen <40068233+ronchen1@users.noreply.github.com>
2022-06-08 11:22:37 +0300

    added computation of metrics on 70% most contiguous natural area

--- R/calc_landscape_metrics.R
+++ R/calc_landscape_metrics.R
@@ -9,9 +9,14 @@ code_table <- data.table(unit_in_filename = c("Batha","Horesh","Coastal","Negev"
                          unit = c("batha","maquis","coastal_sands","negev_highlands","inland_sands",
                                   "conifer_forest","desert_med_trans","loess","arid_south"))
 years <- c("2015","2017","2020")
-output <- data.table(unit=character(), year=numeric(), p_area_mn = numeric(), p_area_sd=numeric(), p_total_area_km2=numeric(),
+output <- data.table(unit=character(), year=numeric(), p_area_mn = numeric(), p_area_sd=numeric(), p_area_tot_km2=numeric(),
                      p_area_max_km2 = numeric(), p_num=numeric(), edge_density_km_km2=numeric(), p_density_n_km2=numeric(),
-                     unit_total_area_km2=numeric(), p_contig_area_km2=list(), p_contig_prop=numeric(), p_data = list())
+                     unit_area_tot_km2=numeric(), p_contig_area_tot_km2=numeric(),
+                     p_contig_prop = numeric(), p_contig_num = numeric(), p_contig_area_mn = numeric(),
+                     p_contig_area_md = numeric(), p_contig_density_n_km2 = numeric(), 
+                     p_contig_edge_dens_km_km2 = numeric())
+
+p_data <- data.table(unit=character(), year=numeric(), id=numeric(), area_km2=numeric(), perim_km=numeric())
 
 # calculate metrics----
 # use <lsm_abbreviations_names %>% print(n=Inf)> or <list_lsm()> to get a list of the metrics abbreviations
@@ -29,44 +34,53 @@ for (i in 1:length(years)) {
     curr_out <- curr_out[class==1L | is.na(class),]
     
     # table of patch data
-    p_data <- dcast.data.table(curr_out[level=="patch",], formula = id ~ metric, value.var = 'value')
-    p_data$area <- p_data$area/100
-    p_data$perim <- p_data$perim/1000
-    p_data <- p_data[order(-area),]
-    setnames(p_data,c("area","perim"),c("area_km2","perim_km"))
+    curr_p_data <- dcast.data.table(curr_out[level=="patch",], formula = id ~ metric, value.var = 'value')
+    curr_p_data$area <- curr_p_data$area/100
+    curr_p_data$perim <- curr_p_data$perim/1000
+    curr_p_data <- curr_p_data[order(-area),]
+    setnames(curr_p_data,c("area","perim"),c("area_km2","perim_km"))
+    curr_p_data <- cbind(data.table(unit=code_table[j,unit],year=years[i]),curr_p_data)
+    p_data <- rbind(p_data,curr_p_data)
     
     # build list of most contiguous patches
-    patch_area_v <- p_data[,area_km2] 
+    patch_area_v <- curr_p_data[,area_km2] 
     pct_v <- patch_area_v/sum(patch_area_v)
     cdf_v <- cumsum(pct_v)
     idx <- match(T,cdf_v>contiguous_area_thr)
-    
+    curr_contig_p <- curr_p_data[1:idx,]
     
     # build new results row
     new_output_row <- data.table(unit=code_table[j,unit],
                                  year=as.numeric(years[i]),
                                  edge_density_km_km2=curr_out[metric=="ed",value]/10,
-                                 unit_total_area_km2=curr_out[metric=="ta",value]/100,
-                                 p_data = list(p_data))
+                                 unit_area_tot_km2=curr_out[metric=="ta",value]/100)
     
-    new_output_row[1, `:=`(p_area_mn = mean(p_area_km2[[1]]),
-                   p_area_sd = sd(p_area_km2[[1]]),
-                   p_total_area_km2=sum(p_area_km2[[1]]),
-                   p_area_max_km2 = max(p_area_km2[[1]]),
-                   p_num = length(p_area_km2[[1]]))]
+    new_output_row[1, `:=`(p_area_mn = mean(curr_p_data[,area_km2]),
+                   p_area_sd = sd(curr_p_data[,area_km2]),
+                   p_area_tot_km2=sum(curr_p_data[,area_km2]),
+                   p_area_max_km2 = max(curr_p_data[,area_km2]),
+                   p_num = length(curr_p_data[,area_km2]),
+                   p_contig_area_tot_km2 = sum(curr_contig_p[,area_km2]),
+                   p_contig_prop = cdf_v[idx],
+                   p_contig_num = idx,
+                   p_contig_area_mn = mean(curr_contig_p[,area_km2]),
+                   p_contig_area_md = median(curr_contig_p[,area_km2]))]
     
-    new_output_row[1,p_density_n_km2 := p_num / unit_total_area_km2]
+    new_output_row[1, `:=`(p_density_n_km2 = p_num / unit_area_tot_km2,
+                           p_contig_density_n_km2 = idx/p_contig_area_tot_km2,
+                           p_contig_edge_dens_km_km2 = sum(curr_contig_p[,perim_km])/p_contig_area_tot_km2)]
     
-    new_output_row[1, `:=`(p_contig_area_km2 = list(patch_area_v[1:idx]),
-                           p_contig_prop = cdf_v[idx],
-                           p_contig_num = idx,
-                           )]
     
     setcolorder(x = new_output_row, neworder = names(output))
     
     output <- rbind(output,new_output_row)
   }
 }
+
+# write data to file----
+
+fwrite(output, file = "Output/results_table_natural_and_forested.csv")
+fwrite(p_data, file = "Output/patch_data.csv")
 
 
 # calculate percent difference in metrics----
@@ -83,12 +97,10 @@ A[,unit:=rep("average",3)]
 setcolorder(x = A, neworder = names(perc_diff))
 perc_diff <- rbind(perc_diff,A)
 
-diff_natural <- output[order(year),lapply(.SD,diff),by=unit, .SDcols = "p_total_area_km2"] %>% cbind(.,data.table(year=c(2017,2020)))
+diff_natural <- output[order(year),lapply(.SD,diff),by=unit, .SDcols = "p_area_tot_km2"] %>% cbind(.,data.table(year=c(2017,2020)))
 print(diff_natural)
 
-# write data to file----
-
-fwrite(output, file = "Output/results_table_natural_and_forested.csv")
+# write data to file
 fwrite(perc_diff, file = "Output/perc_diff_table_natural_and_forested.csv")
 
 # plot results----
